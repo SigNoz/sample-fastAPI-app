f8764b5f7ea44c35584ec0b9c675ccd583ffc87a
makeavish <makeavish786@gmail.com>
2023-01-16 16:28:06 +0530

    add exception endpoint

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 import asyncio
+import logging
+from fastapi import HTTPException, status
+import requests
 import random
+from opentelemetry import trace
+from opentelemetry.trace.status import Status, StatusCode
 
 import requests
 from fastapi import FastAPI
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 random.seed(54321)
-FastAPIInstrumentor().instrument(excluded_urls="ping")
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 app = FastAPI()
 
@@ -35,6 +40,26 @@ async def read_item(item_id: int, q: str = None):
 async def invalid():
     raise ValueError("Invalid ")
 
+@app.get("/exception")
+async def exception():
+    try:
+        raise ValueError("sadness")
+    except Exception as ex:
+        logger.error(ex, exc_info=True)
+        span = trace.get_current_span()
+
+        # generate random number
+        seconds = random.uniform(0, 30)
+
+        # record_exception converts the exception into a span event. 
+        exception = IOError("Failed at " + str(seconds))
+        span.record_exception(exception)
+        span.set_attributes({'est': True})
+        # Update the span status to failed.
+        span.set_status(Status(StatusCode.ERROR, "internal error"))
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Got sadness")
 
 @app.get("/external-api")
 def external_api():
